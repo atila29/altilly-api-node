4ec1dc25ca97fb0755d0bf89b3f61207fcac4fe0
7013145 <tolle_95@hotmail.com>
2018-10-16 17:03:14 +0200

    fix class references in tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -4,17 +4,17 @@ import * as chaiAsPromised from "chai-as-promised";
 import "chai/register-should";
 chai.use(chaiAsPromised);
 
-import HitBTC from './index';
+import AltillyApi from "./index";
 
 
 test("uses the production domain", async () => {
-  const client = new HitBTC({ key: "key", secret: "secret" });
+  const client = new AltillyApi({ key: "key", secret: "secret" });
 
   client.baseUrl.should.equal("https://api.altilly.com/api");
 });
 
 test("uses the passed baseUrl when provided", async () => {
-  const client = new HitBTC({ key: "key", secret: "secret", baseUrl: "example.com" });
+  const client = new AltillyApi({ key: "key", secret: "secret", baseUrl: "example.com" });
 
   client.baseUrl.should.equal("example.com");
 });
